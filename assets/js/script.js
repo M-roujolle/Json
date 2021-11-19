ca68b7b2e63cbf4d8801e6556fb5165b2c42b4db
Maxime <moostafarai@hotmail.fr>
2021-11-19 12:10:56 +0100

    conversion notes en etoiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,81 @@ fetch("assets/json/movies.json")
       //   console.log(element.original_title);
       //   console.log(element.id);
       //   console.log(element["poster_patch"]);
-      document.getElementById(
-        "copy"
-      ).innerHTML += `<div class="col-lg-3 col-8 m-auto" id="${element.id}">
+      console.log();
+      document.getElementById("copy").innerHTML += `
+      <div class="col-lg-3 col-8 m-auto" id="${element.id}">
 
     <div class="row">
 
-        <img class ="img col-lg-5 mt-3" src="${element.poster_path}">
+        <img class ="img col-lg-5 mt-5" src="${element.poster_path}">
 
         <div class="col-lg-7">
 
             <div class="row mt-3">
 
-                <p class="titre">${element.original_title}</p>
+                <p class="titre mt-4">${element.original_title}</p>
                 <p class="text">${element.overview}</p>
-                <p>${element.vote_average}</p>
+                <p class="mt-5 text-warning">${displayStar(
+                  element.vote_average
+                )}</p>
 
             </div>
         </div>
     </div>`;
     });
   });
+
+function displayStar(note) {
+  let vote_average = Number(note) / 2;
+  vote_average = Math.floor(vote_average);
+
+  let stars = "";
+
+  switch (vote_average) {
+    case 1:
+      stars = `<i class="bi bi-star-fill"></i>`;
+      break;
+    case 2:
+      stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`;
+      break;
+    case 3:
+      stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`;
+      break;
+    case 4:
+      stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`;
+      break;
+    case 5:
+      stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`;
+      break;
+
+    default:
+      break;
+  }
+  return stars;
+}
+
+// function displayStar(note) {
+//   let vote_average = Number(note) / 2;
+//   vote_average = Math.floor(vote_average);
+//   let stars = "";
+//   switch (vote_average) {
+//     case 1:
+//       stars = `<i class="bi bi-star-fill"></i>`;
+//       break;
+//     case 2:
+//       stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`;
+//       break;
+//     case 3:
+//       stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`;
+//       break;
+//     case 4:
+//       stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`;
+//       break;
+//     case 5:
+//       stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`;
+//       break;
+//     default:
+//       break;
+//   }
+//   return stars;
+// }
